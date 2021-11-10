4bdfc00520d1bd74949d441817d4e7a1b7eedd7a
Callyel Alves <callyelalves@hotmail.com>
2021-11-10 15:38:58 -0300

    finish blog

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -7,7 +7,7 @@ export default class Comments extends Component {
     script.setAttribute('src', 'https://utteranc.es/client.js');
     script.setAttribute('crossorigin', 'anonymous');
     script.setAttribute('async', 'true');
-    script.setAttribute('repo', 'CallyelAlves/comments');
+    script.setAttribute('repo', 'CallyelAlves/desafio_blog');
     script.setAttribute('issue-term', 'pathname');
     script.setAttribute('theme', 'github-dark');
     anchor.appendChild(script);
